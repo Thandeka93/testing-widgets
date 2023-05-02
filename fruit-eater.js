f942bc92481572713a70f4d31db0c646f033895d
Thandeka93 <noluthandombali4@gmail.com>
2023-05-02 17:10:16 +0200

    testing widgets

---
+++ fruit-eater.js
@@ -0,0 +1,23 @@
+function FruitEater() {
+    var appleCount = 0;
+    var pearCount = 0;
+  
+    return {
+      eat: function(fruit) {
+        if (fruit === 'apple') {
+          appleCount++;
+        } else if (fruit === 'pear') {
+          pearCount++;
+        }
+      },
+  
+      applesEaten: function() {
+        return appleCount;
+      },
+  
+      pearsEaten: function() {
+        return pearCount;
+      }
+    };
+  }
+  
---
+++ tests/fruit-eater.tests.js
@@ -0,0 +1,33 @@
+describe('The FruitEater function ', function(){
+    it('should count how many apples have beed eaten', function(){
+        var appleAndPearEater = FruitEater();
+        appleAndPearEater.eat('apple');
+        appleAndPearEater.eat('apple');
+        assert.equal(appleAndPearEater.applesEaten(), 2);
+    });
+    
+    it('should count how many apples and pears have beed eaten', function(){
+        var appleAndPearEater = FruitEater();
+        appleAndPearEater.eat('apple');
+        appleAndPearEater.eat('pear');
+        appleAndPearEater.eat('apple');
+        appleAndPearEater.eat('pear');
+        appleAndPearEater.eat('apple');
+        assert.equal(appleAndPearEater.applesEaten(), 3);
+        assert.equal(appleAndPearEater.pearsEaten(), 2);
+    });
+
+    it('should count how many pears have beed eaten', function(){
+        var appleAndPearEater = FruitEater();
+        appleAndPearEater.eat('pear');
+        appleAndPearEater.eat('pear');
+        assert.equal(appleAndPearEater.pearsEaten(), 2);
+    });
+
+    it('should return 0 when no fruit has been eaten', function(){
+        var appleAndPearEater = FruitEater();
+        appleAndPearEater.eat('');
+        appleAndPearEater.eat('');
+        assert.equal(appleAndPearEater.pearsEaten(), 0);
+    });
+})
